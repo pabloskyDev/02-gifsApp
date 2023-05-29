c9fbf5d2200ccb144233128203d6523bf5b3ebfb
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2023-05-28 23:49:04 -0500

    [ADD] Add lazy-image component

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html'
+})
+export class LazyImageComponent implements OnInit {
+
+  @Input() url!: string;
+  @Input() alt: string = '';
+  hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if( !this.url ) throw new Error("URL property ir required");
+  }
+
+  onLoad() {
+    this.hasLoaded = true;
+  }
+
+}
