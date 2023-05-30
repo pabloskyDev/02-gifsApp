0709c6a020ee57bf76cbe6e908a319a9c1331d15
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2023-05-30 09:34:02 -0500

    [UPD] Organice routes in files

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
-
 
 
 @NgModule({
